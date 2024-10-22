c0aab8796d579b6ac5d13474bc9605a1447370e4
Hadi <Hadi.rezaie2015@gmail.com>
2024-10-22 11:04:54 +0430

    initial commit

---
+++ AreaOfCircle.py
@@ -0,0 +1,6 @@
+import  math
+r= float(input("Enter the radius of the circle: "))
+# pi= 3.141592653589793
+# area= pi*r*r
+area=math.pi*r**2
+print("The area of the circle is:",area)
---
+++ StudentDetails.py
@@ -0,0 +1,6 @@
+
+studentId= int(input("Enter student ID: "))
+name= input("Enter student name: ")
+marks= float(input("Enter student marks: "))
+
+print("Student ID:", studentId, "Student Name:", name, "Marks:", marks)
